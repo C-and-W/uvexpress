1a16ab9704fac4e6a839b278b4f736ff39ab0700
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2016-04-09 21:08:16 +0800

    push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require underscore
+//= require gmaps/google
 //= require_tree .
