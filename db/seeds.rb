6d2643f213f358331479938972386979827b6493
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2016-04-09 15:45:22 +0800

    geocode

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Terminal.create(name: 'SM Southmall', route: 'Ayala Makati via Skyway', latitude:14.433158, longitude: 121.01259124)
 Terminal.create(name: 'Times st. (in front of Perpetual Help)', route: 'Lawton via Coastal', latitude:14.44807318, longitude: 120.9852165)
+Terminal.create(name: 'Jollibee Casimiro', route: 'Ayala Makati via Skyway', latitude:14.44478504, longitude: 120.99401127)
