b2d69ce478bce0afd848b4aa8ff69ecfc1597005
jacqhernandez <jm.hernandez96@yahoo.com>
2016-04-17 16:59:49 -0200

    fixed origin and destination form in terminal and route maps

--- db/migrate/20160417075542_add_address_to_terminals.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToTerminals < ActiveRecord::Migration
-  def change
-    add_column :terminals, :address, :string
-  end
-end
--- db/migrate/20160417090220_add_count_to_terminals.rb
+++
@@ -1,5 +0,0 @@
-class AddCountToTerminals < ActiveRecord::Migration
-  def change
-    add_column :terminals, :Count, :integer
-  end
-end
